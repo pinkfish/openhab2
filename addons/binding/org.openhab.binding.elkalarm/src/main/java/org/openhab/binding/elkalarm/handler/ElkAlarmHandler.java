f403307de91cdc91688aefbd5b1d69f545acc0da
David Bennett <ddt@google.com>
2017-02-17 17:34:05 -0800

    First pass at the elk code.

--- addons/binding/org.openhab.binding.elkalarm/src/main/java/org/openhab/binding/elkalarm/handler/ElkAlarmHandler.java
+++ addons/binding/org.openhab.binding.elkalarm/src/main/java/org/openhab/binding/elkalarm/handler/ElkAlarmHandler.java
@@ -7,41 +7,39 @@
  */
 package org.openhab.binding.elkalarm.handler;
 
-import static org.openhab.binding.elkalarm.ElkAlarmBindingConstants.*;
+import static org.openhab.binding.elkalarm.ElkAlarmBindingConstants.CHANNEL_1;
 
+import org.eclipse.smarthome.core.thing.Bridge;
 import org.eclipse.smarthome.core.thing.ChannelUID;
-import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.ThingStatus;
-import org.eclipse.smarthome.core.thing.binding.BaseThingHandler;
+import org.eclipse.smarthome.core.thing.binding.BaseBridgeHandler;
 import org.eclipse.smarthome.core.types.Command;
+import org.openhab.binding.elkalarm.internal.config.ElkAlarmConfig;
+import org.openhab.binding.elkalarm.internal.elk.ElkAlarmConnection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * The {@link ElkAlarmHandler} is responsible for handling commands, which are
  * sent to one of the channels.
- * 
+ *
  * @author David Bennett - Initial contribution
  */
-public class ElkAlarmHandler extends BaseThingHandler {
+public class ElkAlarmHandler extends BaseBridgeHandler {
 
     private Logger logger = LoggerFactory.getLogger(ElkAlarmHandler.class);
 
-	public ElkAlarmHandler(Thing thing) {
-		super(thing);
-	}
+    private ElkAlarmConnection connection;
 
-	@Override
-	public void handleCommand(ChannelUID channelUID, Command command) {
-        if(channelUID.getId().equals(CHANNEL_1)) {
-            // TODO: handle command
+    public ElkAlarmHandler(Bridge thing) {
+        super(thing);
+    }
 
-            // Note: if communication with thing fails for some reason,
-            // indicate that by setting the status with detail information
-            // updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR,
-            // "Could not control device at IP address x.x.x.x");
+    @Override
+    public void handleCommand(ChannelUID channelUID, Command command) {
+        if (channelUID.getId().equals(CHANNEL_1)) {
         }
-	}
+    }
 
     @Override
     public void initialize() {
@@ -49,11 +47,8 @@ public class ElkAlarmHandler extends BaseThingHandler {
         // Long running initialization should be done asynchronously in background.
         updateStatus(ThingStatus.ONLINE);
 
-        // Note: When initialization can NOT be done set the status with more details for further
-        // analysis. See also class ThingStatusDetail for all available status details.
-        // Add a description to give user information to understand why thing does not work
-        // as expected. E.g.
-        // updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR,
-        // "Can not access device as username and/or password are invalid");
+        // Load up the config and then get the connection to the elk setup.
+        ElkAlarmConfig config = getConfigAs(ElkAlarmConfig.class);
+        connection = new ElkAlarmConnection(config);
     }
 }
