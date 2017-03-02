a24f98065caf24937897b033b1c0bdde1cdd07ee
David Bennett <ddt@google.com>
2017-03-02 12:03:01 -0800

    Remove extranous files

--- addons/binding/org.openhab.binding.blueiris/src/main/java/org/openhab/binding/blueiris/handler/BridgeListener.java
+++
@@ -1,9 +0,0 @@
-package org.openhab.binding.blueiris.handler;
-
-import org.openhab.binding.blueiris.internal.data.CamListReply;
-
-public interface BridgeListener {
-
-    void onCamList(CamListReply camListReply);
-
-}
